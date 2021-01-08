29184ac759f6a803ee6e7f01bc44eedff9d24efc
Iris Leite Vieira Junior <irisjunior1@gmail.com>
2021-01-08 13:42:53 -0300

    Add files via upload

---
+++ curso rocketseat/aula javascript/exercicios.js
@@ -0,0 +1,24 @@
+// 1 declare uma variavel de nome weigth.
+let weight
+// 2 que tipo de dado é a variavel acima?
+console.log(typeof weight)
+
+/*
+3. declare uma variavel e atribua valores para cada um dos dados:
+    * name:string   
+    * age: Number (integer)
+    * stars: Number (Float)
+    * isSubscribed: Boolean
+*/
+/* isso e exemplo de objeto:
+  let perfil1 = {
+    name: "junior",
+    age: 34,
+    stars:3.6,
+    issubscribed: true
+}
+*/
+    name = "junior"1
+    age = 34
+    stars = 3.6
+    issubscribed = true
---
+++ curso rocketseat/aula javascript/scripts.js
@@ -0,0 +1,28 @@
+
+
+console.log("ola mundo!!")
+console.log("ola!!")
+
+let name="junior"
+
+
+
+const person = {
+    name:`junior`,
+    idade:34,
+    peso:98.2
+}
+
+console.log(person.name)
+console.log(`o nome dele é ${person.name} e ele  tem ${person.idade} anos`)
+
+//Array
+
+const animals = [
+    `leao`,
+    `zebra`,
+    `vaca`
+]
+    console.log(animals[0])
+    console.log(animals[1])
+    console.log(animals.length)//informa a quantidade de elementos no array.
