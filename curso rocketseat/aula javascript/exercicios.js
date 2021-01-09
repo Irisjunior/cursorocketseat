e7b7329f494da70f04fdca413d832aa47cfbcac0
Iris Leite Vieira Junior <irisjunior1@gmail.com>
2021-01-09 00:54:06 -0300

    Update exercicios.js
    exercicio 4

--- curso rocketseat/aula javascript/exercicios.js
+++ curso rocketseat/aula javascript/exercicios.js
@@ -1,7 +1,7 @@
 // 1 declare uma variavel de nome weigth.
-let weight
+//let weight
 // 2 que tipo de dado é a variavel acima?
-console.log(typeof weight)
+//console.log(typeof weight)
 
 /*
 3. declare uma variavel e atribua valores para cada um dos dados:
@@ -18,7 +18,26 @@ console.log(typeof weight)
     issubscribed: false
 }
 */
-    name = "junior"1
-    age = 34
-    stars = 3.6
-    issubscribed = true
+    //name = "junior"
+    //age = 34
+    //stars = 3.6
+    //issubscribed = true
+
+    //  4. A variavel abaixo e de que tipo de dado?
+        //a. atribua as mesmas propriedades do exercicio 3.
+        //b.mostre no console a seguinte mensagem:
+          //<name>de idade <age> pesa <weight> kg.
+          
+
+    var student = {
+      name: "junior",
+      age: 34,
+      weight: 3.6,
+      issubscribed : true,
+    
+  }
+    console.log(typeof student)
+    console.log(`${student.name} de idade ${student.age} pesa ${student.weight} kg.`)
+
+
+    
