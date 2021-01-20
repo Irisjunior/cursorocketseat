a30c27e8d33c7f06b792f8b20855a91681169ce7
Iris Leite Vieira Junior <irisjunior1@gmail.com>
2021-01-20 15:54:21 -0300

    celsius
    inicio do exercicio

---
+++ exercicios_javascript/CelsiusTofahrenheit.js
@@ -0,0 +1,19 @@
+/*
+    Crie uma Funcao que receba uma string em celsius ou em fahrenheit e faça a transformação de uma unidade para outra
+
+    formula para celsius:(30 °C × 9/5) + 32 = 86 °F
+
+
+*/
+ 
+let celsius = "30.0"
+
+function conversao(let){
+    let * (9/5) + 32
+    return valor
+}
+
+conversao(celsius)
+
+
+console.log(30*(9/5)+32)
